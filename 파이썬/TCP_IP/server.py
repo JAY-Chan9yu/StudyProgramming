a3052f08f3520ee1fcb365e92636d1b78b2d50ca
JAY-Chan9yu <ckj9014@gmail.com>
2018-02-10 22:44:58 +0900

    [채팅 프로그램 비동기 추가하기] -버그 : 소켓 접속하고 나서 select 문에서 소켓이외의 접근을 시도했다고 나옴.

--- 파이썬/TCP_IP/server.py
+++ 파이썬/TCP_IP/server.py
@@ -1,23 +1,74 @@
-import socket
+# -*- coding: utf8 -*-
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12222
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host, port))
+# socket 과 select 모듈 임포트
+from socket import *
+from select import *
+import sys
+from time import ctime
 
-s.listen(5)
-c = None
 
-while True:
-   if c is None:
-       # Halts
-       print( '[Waiting for connection...]')
-       c, addr = s.accept() #  (socket object, address info) return
-       print( 'Got connection from', addr)
-   else:
-       # Halts
-       print( '[Waiting for response...]')
-       print((c.recv(1024)).decode('utf-8'))
-       q = input("Enter something to this client: ")
-       c.send(q.encode('utf-8'))
+# 호스트, 포트와 버퍼 사이즈를 지정
+HOST = ''
+PORT = 56789
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+# 소켓 객체를 만들고..
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# 서버 정보를 바인딩
+serverSocket.bind(ADDR)
+
+# 요청을 기다림(listen)
+serverSocket.listen(10)
+connection_list = [serverSocket]
+print('==============================================')
+print('채팅 서버를 시작합니다. %s 포트로 접속을 기다립니다.' % str(PORT))
+print('==============================================')
+
+# 무한 루프를 시작
+while connection_list:
+    try:
+        print('[INFO] 요청을 기다립니다...')
+
+        # select 로 요청을 받고, 10초마다 블럭킹을 해제하도록 함
+        read_socket, write_socket, error_socket = select(connection_list, [], [], 10)
+
+        for sock in read_socket:
+            # 새로운 접속
+            if sock == serverSocket:
+                clientSocket, addr_info = serverSocket.accept()
+                connection_list.append(clientSocket)
+                print('[INFO][%s] 클라이언트(%s)가 새롭게 연결 되었습니다.' % (ctime(), addr_info[0]))
+
+                # 클라이언트로 응답을 돌려줌
+                for socket_in_list in connection_list:
+                    if socket_in_list != serverSocket and socket_in_list != sock:
+                        try:
+                            socket_in_list.send('[%s] 새로운 방문자가 대화방에 들어왔습니다. 반가워요~ 짝짝짝!' % ctime())
+                        except Exception as e:
+                            socket_in_list.close()
+                            connection_list.remove(socket_in_list)
+            # 접속한 사용자(클라이언트)로부터 새로운 데이터 받음
+            else:
+                data = sock.recv(BUFSIZE)
+                if data:
+                    print('[INFO][%s] 클라이언트로부터 데이터를 전달 받았습니다.' % ctime())
+                    for socket_in_list in connection_list:
+                        if socket_in_list != serverSocket and socket_in_list != sock:
+                            try:
+                                socket_in_list.send('[%s] %s' % (ctime(), data))
+                                print('[INFO][%s] 클라이언트로 데이터를 전달합니다.' % ctime())
+                            except Exception as e:
+                                print(e.message)
+                                socket_in_list.close()
+                                connection_list.remove(socket_in_list)
+                                continue
+                else:
+                    connection_list.remove(sock)
+                    sock.close()
+                    print('[INFO][%s] 사용자와의 연결이 끊어졌습니다.' % ctime())
+    except KeyboardInterrupt:
+        # 부드럽게 종료하기
+        serverSocket.close()
+        sys.exit()
